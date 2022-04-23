dcc0aa185645f292e19c80364b0c923a536c330e
Mario Martinez <francisco.smtz@gmail.com>
2022-04-23 00:12:34 -0500

    fix(seed) Default returned github users.

--- scripts/seed.py
+++ scripts/seed.py
@@ -9,14 +9,11 @@ def main():
     if len(sys.argv) >= 2:
         users_option = sys.argv[1]
         users_number = sys.argv[2]
-    else:
-        print("Wrong arguments, you need to specify the number of users with option -t <total> or --total <total>")
-
-    if users_option and users_number:
-        if users_option in  ["-t", "--total"] and users_number.isdigit():
-            return int(users_number)
-        else:
-            print("Wrong arguments, you need to specify the number of users with option -t <total> or --total <total>")
+        if users_option and users_number:
+            if users_option in  ["-t", "--total"] and users_number.isdigit():
+                return int(users_number)
+            else:
+                print("Wrong arguments, you need to specify the number of users with option -t <total> or --total <total>")
     return 150
     
     
@@ -27,6 +24,8 @@ def get_users(users_number: int):
 
 def populate_database(users_number: int):
     pass
+
+
 
 
 if __name__ == "__main__":
